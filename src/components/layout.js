04a3c829a7d74cba338360839a4570b84ebd96e1
Eliseu Videira <eliseuvideira1@gmail.com>
2020-06-03 23:05:20 -0300

    Switching to functional components

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,20 +1,14 @@
 import React from "react"
 
-class Layout extends React.Component {
-  render() {
-    const { title, subtitle, children } = this.props
-
-    return (
-      <div className="app-container">
-        <header>
-          <h1>{title}</h1>
-          <p>{subtitle}</p>
-        </header>
-        <main>{children}</main>
-        <footer>© {new Date().getFullYear()}, Rachelle Rathbone</footer>
-      </div>
-    )
-  }
+export default function Layout({ title, subtitle, children }) {
+  return (
+    <div className="app-container">
+      <header>
+        <h1>{title}</h1>
+        <p>{subtitle}</p>
+      </header>
+      <main>{children}</main>
+      <footer>© {new Date().getFullYear()}, Rachelle Rathbone</footer>
+    </div>
+  )
 }
-
-export default Layout
