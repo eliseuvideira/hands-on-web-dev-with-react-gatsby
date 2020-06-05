a4b9e48914e8e98fa173e7fe4c1c7e5a2384f344
Eliseu Videira <eliseuvideira1@gmail.com>
2020-06-04 23:28:44 -0300

    Updating SEO using useStaticQuery hook

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -1,36 +1,56 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import { useStaticQuery, graphql } from 'gatsby';
 
 interface Props {
   title: string;
   lang: string;
-  description: string;
+  description?: string;
   meta?: any[];
 }
 
-const SEO: React.FC<Props> = ({ title, lang, description, meta }) => (
-  <Helmet
-    title={title}
-    htmlAttributes={{ lang }}
-    meta={[
-      {
-        name: 'description',
-        content: description,
-      },
-      {
-        property: 'og:title',
-        content: title,
-      },
-      {
-        property: 'og:description',
-        content: description,
-      },
-      {
-        property: 'og:type',
-        content: 'website',
-      },
-    ].concat(meta || [])}
-  ></Helmet>
-);
+const SEO: React.FC<Props> = ({ title, lang, description, meta }) => {
+  const {
+    site: { siteMetadata },
+  } = useStaticQuery(graphql`
+    {
+      site {
+        siteMetadata {
+          author
+          description
+        }
+      }
+    }
+  `);
+
+  return (
+    <Helmet
+      title={title}
+      htmlAttributes={{ lang }}
+      meta={[
+        {
+          name: 'description',
+          content: description,
+        },
+        {
+          name: 'author',
+          content: siteMetadata.author,
+        },
+        {
+          property: 'og:title',
+          content: title,
+        },
+        {
+          property: 'og:description',
+          content: description || siteMetadata.description,
+        },
+        {
+          property: 'og:type',
+          content: 'website',
+        },
+      ].concat(meta || [])}
+    ></Helmet>
+  );
+};
 
 export default SEO;
